e957f563f4abed4c06e79cdf8780e40575fec20a
ugnoticiasmineras <ugnoticiasmineras@gmail.com>
2025-12-31 16:54:41 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
+  // Puedes agregar otras configuraciones aquí si ya las usas
+})
