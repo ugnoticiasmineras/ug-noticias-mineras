026a58d805b5d96e864ccb6c169314294c08b6a2
ugnoticiasmineras <ugnoticiasmineras@gmail.com>
2025-12-31 17:11:31 -0300

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0056b3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
